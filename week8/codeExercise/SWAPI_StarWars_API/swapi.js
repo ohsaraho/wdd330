2a7734c92e64f50880512aeff4ecc5f1a245e32e
ohsaraho <osg21001@byui.edu>
2022-06-11 22:18:43 -0600

    added show details

--- week8/codeExercise/SWAPI_StarWars_API/swapi.js
+++ week8/codeExercise/SWAPI_StarWars_API/swapi.js
@@ -13,21 +13,45 @@ function getAPI(url = "https://swapi.dev/api/starships/") {
       });
 }
 
-function renderShipArray(ships) {
-  const array = document.querySelector('.tbody');
-  array.innerHTML = '';
-
-  ships.forEach(ship => {
-    let listItem = document.createElement('tr');
-    listItem.innerHTML = `<td>${ship.name}</td>
-                          <td>${ship.model}</td>
-                          <td>${ship.manufacturer}</td>`;
+  function renderShipArray(ships) {
+    const array = document.querySelector('.tbody');
+    array.innerHTML = '';
+  
+    ships.forEach(ship => {
+      let listItem = document.createElement('tr');
+      listItem.innerHTML = `<td><a href="${ship.url}">${ship.name}</a></td>
+                            <td>${ship.model}</td>
+                            <td>${ship.manufacturer}</td>`;
+      
+        listItem.addEventListener("click", function (event) {
+        //when clicked the default link behavior should be stopped, and the ship details function should be called...passing the value of the href attribute in
+            event.preventDefault();
+            getShipDetails(ship.url);
+        });
+      array.appendChild(listItem);
+    });
+  }
+  
+  function renderShipDetails(shipData) {
+    console.log(shipData);
+    const array = document.querySelector('#detailsBox');
+    array.innerHTML = '';
+    array.classList.remove('hidden');
     
-    array.appendChild(listItem);
-  });
-}
-
-function showShips(url) {
+      let listItem = document.createElement('div');
+      listItem.innerHTML = `<div><dt>Name:</dt>
+      <dd class="name">${shipData.name}</dd></div>
+      <div><dt>Model:</dt>
+      <dd class="model">${shipData.model}</dd></div>
+      <div><dt>Length:</dt>
+      <dd class="class">${shipData.length}</dd></div>
+      <div><dt>Manufacturer:</dt>
+      <dd class="movies">${shipData.crew}</dd></div>`;
+      
+      array.appendChild(listItem);
+  }
+  
+  function showShips(url) {
   getAPI(url).then((data) => {
     const results = data.results;
     console.log(results);
@@ -35,6 +59,7 @@ function showShips(url) {
 
 
     renderShipArray(results);
+    renderShipDetails(results);
 
     if (data.next) {
       const next = document.getElementById("next");
@@ -53,7 +78,12 @@ function showShips(url) {
     
   })
 }
-
-
-
+  
+  function getShipDetails(url) {
+    //call getJSON functions for the provided url
+    getAPI(url).then(function (data) {
+      renderShipDetails(data);
+      //with the results populate the elements in the #detailsbox
+    });
+}
   showShips();
