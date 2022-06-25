1fad8e0141867cbd5b00ae2c50120480a169e028
ohsaraho <osg21001@byui.edu>
2022-06-25 17:08:38 -0600

    finished half of the json data

--- finalProject/js/fetchDogBreeds.js
+++ finalProject/js/fetchDogBreeds.js
@@ -16,17 +16,17 @@ export function getJSON(dogDatajson = 'json/dogBreedsData.json') {
             console.table(dogData);
             dogData.forEach(data => { 
                 let card = document.createElement('section');
-                let p = document.createElement('p');
+                // let p = document.createElement('p');
                 let image1 = document.createElement('img');
                 let image2 = document.createElement('img');
         
-            p.textContent = data.lifeSpan;    
+            // p.textContent = data.lifeSpan;    
             image1.setAttribute('src', `images/${data.image[0].image1}`);
             image2.setAttribute('src', `images/${data.image[0].image2}`);
             image1.setAttribute('alt', data.dogBreed);
             image2.setAttribute('alt', data.dogBreed);
 
-            card.appendChild(p);
+            // card.appendChild(p);
             card.appendChild(image1);
             card.appendChild(image2);
             
