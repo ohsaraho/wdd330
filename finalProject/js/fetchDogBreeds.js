4963be9d8f27bca8620f6f136dfbb264898f8d25
ohsaraho <osg21001@byui.edu>
2022-06-25 21:30:13 -0600

    finished json

--- finalProject/js/fetchDogBreeds.js
+++ finalProject/js/fetchDogBreeds.js
@@ -11,28 +11,28 @@ export function getJSON(dogDatajson = 'json/dogBreedsData.json') {
                 return response.json();
             }
         })
-        .then(function (jsonObject) {
-            const dogData = jsonObject['dogData'];
-            console.table(dogData);
-            dogData.forEach(data => { 
-                let card = document.createElement('section');
-                // let p = document.createElement('p');
-                let image1 = document.createElement('img');
-                let image2 = document.createElement('img');
+    //     .then(function (jsonObject) {
+    //         const dogData = jsonObject['dogData'];
+    //         console.table(dogData);
+    //         dogData.forEach(data => { 
+    //             let card = document.createElement('section');
+    //             // let p = document.createElement('p');
+    //             let image1 = document.createElement('img');
+    //             let image2 = document.createElement('img');
         
-            // p.textContent = data.lifeSpan;    
-            image1.setAttribute('src', `images/${data.image[0].image1}`);
-            image2.setAttribute('src', `images/${data.image[0].image2}`);
-            image1.setAttribute('alt', data.dogBreed);
-            image2.setAttribute('alt', data.dogBreed);
+    //         // p.textContent = data.lifeSpan;    
+    //         image1.setAttribute('src', `images/${data.image[0].image1}`);
+    //         image2.setAttribute('src', `images/${data.image[0].image2}`);
+    //         image1.setAttribute('alt', data.dogBreed);
+    //         image2.setAttribute('alt', data.dogBreed);
 
-            // card.appendChild(p);
-            card.appendChild(image1);
-            card.appendChild(image2);
+    //         // card.appendChild(p);
+    //         card.appendChild(image1);
+    //         card.appendChild(image2);
             
 
 
-            document.querySelector('.cards').appendChild(card);
-        })
-    })
+    //         document.querySelector('.cards').appendChild(card);
+    //     })
+    // })
 }
--- finalProject/js/matchDogBreed.js
+++ finalProject/js/matchDogBreed.js
@@ -1,5 +1,32 @@
 import { getJSON } from './fetchDogBreeds.js';
 
+function dogArray() {
+    getJSON().then(function (jsonObject) {
+        const dogData = jsonObject['dogData'];
+        console.table(dogData);
+        dogData.forEach(data => { 
+            let card = document.createElement('div');
+            // let p = document.createElement('p');
+            let image1 = document.createElement('img');
+            let image2 = document.createElement('img');
+    
+        // p.textContent = data.lifeSpan;    
+        image1.setAttribute('src', `images/${data.image[0].image1}`);
+        image2.setAttribute('src', `images/${data.image[0].image2}`);
+        image1.setAttribute('alt', data.dogBreed);
+        image2.setAttribute('alt', data.dogBreed);
+    
+        // card.appendChild(p);
+        card.appendChild(image1);
+        card.appendChild(image2);
+        
+    
+    
+        document.querySelector('.cards').appendChild(card);
+    })
+    })
+}
 
 
-getJSON();
+dogArray();
+// getJSON();
