1065dfde6e770f1cee87d4cd80f3de9cc7311501
ohsaraho <osg21001@byui.edu>
2022-07-02 01:19:32 -0600

    beginning stages of the memory game

--- finalProject/js/matchDogBreed.js
+++ finalProject/js/matchDogBreed.js
@@ -1,72 +1,301 @@
 import { getJSON } from './fetchDogBreeds.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-function dogArray() {
-    // getJSON().then(function (jsonObject) {
-    //     const dogData = jsonObject['dogData'];
-    //     console.table(dogData);
-    //     dogData.forEach(data => { 
-    //         let card = document.createElement('div');
-    //         // let p = document.createElement('p');
-    //         let image1 = document.createElement('img');
-    //         let image2 = document.createElement('img');
+// function dogArray() {
+//     getJSON().then(function (jsonObject) {
+//         const dogData = jsonObject['dogData'];
+//         console.table(dogData);
+//         dogData.forEach(data => { 
+//             let card = document.createElement('div');
+//             // let p = document.createElement('p');
+//             let image1 = document.createElement('img');
+//             let image2 = document.createElement('img');
     
-    //     // p.textContent = data.lifeSpan;    
-    //     image1.setAttribute('src', `images/${data.image[0].image1}`);
-    //     image2.setAttribute('src', `images/${data.image[0].image2}`);
-    //     image1.setAttribute('alt', data.dogBreed);
-    //     image2.setAttribute('alt', data.dogBreed);
+//         // p.textContent = data.lifeSpan;    
+//         image1.setAttribute('src', `images/${data.image[0].image1}`);
+//         image2.setAttribute('src', `images/${data.image[0].image2}`);
+//         image1.setAttribute('alt', data.dogBreed);
+//         image2.setAttribute('alt', data.dogBreed);
     
-    //     // card.appendChild(p);
-    //     card.appendChild(image1);
-    //     card.appendChild(image2);
+//         // card.appendChild(p);
+//         card.appendChild(image1);
+//         card.appendChild(image2);
         
     
     
-    //     document.querySelector('.cards').appendChild(card);
-    // })
-    // })
-    // const array = [];
+//         document.querySelector('.cards').appendChild(card);
+//     })
+//     })
+//     const array = [];
 
-    getJSON().then(function (jsonObject) {
-        const dogData = jsonObject['dogData'];
-        dogData.forEach(data => {
-            const cardArray = [
-                {
-                    name: 'berneseDog',
-                    img: `images/${data.image[0].image1}`
-                },
-                {
-                    name: 'berneseDog',
-                    img: `images/${data.image[0].image1}`
-                },
-                {
-                    name: 'bernesePup',
-                    img: `images/${data.image[0].image2}`
-                },
-                {
-                    name: 'bernesePup',
-                    img: `images/${data.image[0].image2}`
-                }
-            ];
-            return cardArray;
-        })
-    })
-}
+//     getJSON().then(function (jsonObject) {
+//         const dogData = jsonObject['dogData'];
+//         console.log(dogData);
+//         dogData.forEach(data => {
+//             const cardArray = [
+//                 {
+//                     name: 'berneseDog',
+//                     img: `images/${data.image[0].image1}`
+//                 },
+//                 {
+//                     name: 'berneseDog',
+//                     img: `images/${data.image[0].image1}`
+//                 },
+//                 {
+//                     name: 'bernesePup',
+//                     img: `images/${data.image[0].image2}`
+//                 },
+//                 {
+//                     name: 'bernesePup',
+//                     img: `images/${data.image[0].image2}`
+//                 }
+//             ];
+//             return cardArray;
+//         })
+//     })
+// }
 
-
-dogArray();
+getJSON().then((data) => {
+    // const dogData = jsonObject['dogData'];
+    console.log(data);
+    const dogData = data.dogData[0].images[0].image;
+    console.log(dogData);
+    // dogData.forEach(data => {
+                    const cardArray = [
+                        {
+                            name: `${data.dogData[0].images[0].imageName}`,
+                            img: `images/${data.dogData[0].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[0].images[0].imageName}`,
+                            img: `images/${data.dogData[0].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[0].images[1].imageName}`,
+                            img: `images/${data.dogData[0].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[0].images[1].imageName}`,
+                            img: `images/${data.dogData[0].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[1].images[0].imageName}`,
+                            img: `images/${data.dogData[1].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[1].images[0].imageName}`,
+                            img: `images/${data.dogData[1].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[1].images[1].imageName}`,
+                            img: `images/${data.dogData[1].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[1].images[1].imageName}`,
+                            img: `images/${data.dogData[1].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[2].images[0].imageName}`,
+                            img: `images/${data.dogData[2].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[2].images[0].imageName}`,
+                            img: `images/${data.dogData[2].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[2].images[1].imageName}`,
+                            img: `images/${data.dogData[2].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[2].images[1].imageName}`,
+                            img: `images/${data.dogData[2].images[1].image}`
+                        },{
+                            name: `${data.dogData[3].images[0].imageName}`,
+                            img: `images/${data.dogData[3].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[3].images[0].imageName}`,
+                            img: `images/${data.dogData[3].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[3].images[1].imageName}`,
+                            img: `images/${data.dogData[3].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[3].images[1].imageName}`,
+                            img: `images/${data.dogData[3].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[4].images[0].imageName}`,
+                            img: `images/${data.dogData[4].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[4].images[0].imageName}`,
+                            img: `images/${data.dogData[4].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[4].images[1].imageName}`,
+                            img: `images/${data.dogData[4].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[4].images[1].imageName}`,
+                            img: `images/${data.dogData[4].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[5].images[0].imageName}`,
+                            img: `images/${data.dogData[5].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[5].images[0].imageName}`,
+                            img: `images/${data.dogData[5].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[5].images[1].imageName}`,
+                            img: `images/${data.dogData[5].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[5].images[1].imageName}`,
+                            img: `images/${data.dogData[5].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[6].images[0].imageName}`,
+                            img: `images/${data.dogData[6].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[6].images[0].imageName}`,
+                            img: `images/${data.dogData[6].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[6].images[1].imageName}`,
+                            img: `images/${data.dogData[6].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[6].images[1].imageName}`,
+                            img: `images/${data.dogData[6].images[1].image}`
+                        },{
+                            name: `${data.dogData[7].images[0].imageName}`,
+                            img: `images/${data.dogData[7].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[7].images[0].imageName}`,
+                            img: `images/${data.dogData[7].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[7].images[1].imageName}`,
+                            img: `images/${data.dogData[7].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[7].images[1].imageName}`,
+                            img: `images/${data.dogData[7].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[8].images[0].imageName}`,
+                            img: `images/${data.dogData[8].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[8].images[0].imageName}`,
+                            img: `images/${data.dogData[8].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[8].images[1].imageName}`,
+                            img: `images/${data.dogData[8].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[8].images[1].imageName}`,
+                            img: `images/${data.dogData[8].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[9].images[0].imageName}`,
+                            img: `images/${data.dogData[9].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[9].images[0].imageName}`,
+                            img: `images/${data.dogData[9].images[0].image}`
+                        },
+                        {
+                            name: `${data.dogData[9].images[1].imageName}`,
+                            img: `images/${data.dogData[9].images[1].image}`
+                        },
+                        {
+                            name: `${data.dogData[9].images[1].imageName}`,
+                            img: `images/${data.dogData[9].images[1].image}`
+                        }
+                    ];
+        console.log(cardArray);
+                
+// dogArray();
 // getJSON();
+// const cardArray = [
+//     {
+//         name: 'berneseDog',
+//         img: 'images/bernese-mountain-dog.jpg'
+//     },
+//     {
+//         name: 'berneseDog',
+//         img: 'images/bernese-mountain-dog.jpg'
+//     },
+//     {
+//         name: 'bernesePup',
+//         img: 'images/bernese-mountain-puppy.jpg'
+//     },
+//     {
+//         name: 'bernesePup',
+//         img: 'images/bernese-mountain-puppy.jpg'
+//     }
+// ];
 
+    // cardArray.sort(() => 0.5 - Math.random());
     const cards = document.querySelector('.cards');
+
+    let cardsSelected = [];
+    let cardsSelectedId = [];
+    let cardsWon = [];
 
     function createBoard() {
         for (let i=0; i < cardArray.length; i++) {
             var card = document.createElement('img');
             card.setAttribute('src', 'images/front.png');
             card.setAttribute('data-id', i);
+            card.addEventListener('click', flipSelectedCard)
             cards.appendChild(card);
         }
     }
+
+    function checkForCardMatch() {
+        const cards = document.querySelectorAll('img');
+        const selectedCard1Id = cardsSelectedId[0];
+        const selectedCard2Id = cardsSelectedId[1];
+
+        if (cardsSelected[0] == cardsSelected[1]) {
+            const wonMessage = document.createElement('h2');
+            wonMessage.innerHTML = 'You found a match';
+            cards[selectedCard1Id].setAttribute('src', 'images/blank.jpg');
+            cards[selectedCard2Id].setAttribute('src', 'images/blank.jpg');
+            cards
+            cardsWon.push(cardsSelected);
+        } else {
+            cards[selectedCard1Id].setAttribute('src', 'images/front.png');
+            cards[selectedCard2Id].setAttribute('src', 'images/front.png');
+            const lossMessage = document.createElement('h2');
+            lossMessage.innerHTML = 'Sorry no match. Try again!';
+        }
+        cardsSelected = []
+        cardsSelectedId = []
+    }
+
+    function flipSelectedCard() {
+        const cardId = this.getAttribute('data-id');
+        cardsSelected.push(cardArray[cardId].name);
+        cardsSelectedId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (cardsSelected.length == 2) {
+            setTimeout(checkForCardMatch, 500);
+        }
+    }
+
+
     createBoard();
+    cards.forEach(card => card.addEventListener('click', flipSelectedCard));
+// })
+})
 })
